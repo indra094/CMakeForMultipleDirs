08c5de6b32f896b60f76444ece52ce03b8ef1d4e
indra094 <ij.nandy10@gmail.com>
2021-08-03 17:44:20 +0530

    Read inheritance relationship from file but for only one instance. Need to setup SQLite and boost libraries next.

--- main.cpp
+++ main.cpp
@@ -13,14 +13,48 @@ int main()
 	cout << "folder 1 says "; objFromFolder1.sayHello();
 	cout << "\nfolder 2 says "; objFromFolder2.sayHello();
 
-	ifstream infile("../main.cpp");
+	ifstream infile("../Hierarchy.txt");
 	ostringstream contentStream;
 	contentStream<<infile.rdbuf();
 	infile.close();
 
+	string contents = contentStream.str();
+	int i = 0;
+	string baseClass, derivedClass;
+	bool relationshipParseDone = false;
+	while(contents[i]!='\0')
+	{
+		switch(contents[i])
+		{
+		case '-':
+		{
+		   if(contents[i+1] == '>')
+		   {
+		      relationshipParseDone = true;
+		      i++;
+		   }
+			break;
+		}
+		default:
+		{
+		   if(relationshipParseDone)
+		   {
+		      baseClass.push_back(contents[i]);
+		   }
+		   else
+		   {
+		      derivedClass.push_back(contents[i]);
+		   }
+		   break;
+		}
+		}
+		i++;
+	}
+	cout<<baseClass<<" is parent of "<<derivedClass;
+	/*
 	ofstream out_file("../main.cpp");
 	out_file<<contentStream.str()+"boomchika";
-	out_file.close();
+	out_file.close();*/
 
 	return 0;
 }
