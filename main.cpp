13a5d2a3a8d6b3f2839d0f4d7fde12e13e2ac191
indra094 <ij.nandy10@gmail.com>
2021-08-03 17:00:51 +0530

    Added code to alter own code(adds the word boomchika currently)

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
-#include<iostream>
-#include"Folder1/header1.h"
-#include"Folder2/header2.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include "Folder1/header1.h"
+#include "Folder2/header2.h"
 
 using namespace std;
 
@@ -10,5 +12,15 @@ int main()
 	H2 objFromFolder2;
 	cout << "folder 1 says "; objFromFolder1.sayHello();
 	cout << "\nfolder 2 says "; objFromFolder2.sayHello();
+
+	ifstream infile("../main.cpp");
+	ostringstream contentStream;
+	contentStream<<infile.rdbuf();
+	infile.close();
+
+	ofstream out_file("../main.cpp");
+	out_file<<contentStream.str()+"boomchika";
+	out_file.close();
+
 	return 0;
 }
