b1e0f156b027dfbf6a8ae39ca1968b45d9063959
indra094 <ij.nandy10@gmail.com>
2021-08-07 14:21:26 +0530

    So I think I understand whats happening.\n1.I was able to use boost in my code which is important but I still need to get the linking part with cmake right.\n2.I was able to use that part of boost which did not require separate building like boost::lambda. It was important to alter the cmakelists.txt file and add it in include_directories.\n3. Now though I might or might not use boost in this project, it is important to know how to link an externally built library to my code and with cmake here. This will create a general understanding of things and I can be comfortable with externally built libraries as well.\n 4. To use Boost::Filesystem I need to build boost libraries separately and then link it to my program. I have not yet done this but I believe I need to update target_link_libraries in the CMakeLists.txt file with the library name and I need to update target_include_directories() in it with the directory containing the library. Lets see if I'm correct.\n 5. If I get this right it should be fairly similar to setup any other external built libraries.\n6. Path variables are needed to be altered when I am running another command such as b2 in this case.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <iterator>
+#include <algorithm>
+#include <boost/lambda/lambda.hpp>
 #include "Folder1/header1.h"
 #include "Folder2/header2.h"
 
 using namespace std;
+
+void createFile(string filePath)
+{
+   std::ofstream file(filePath);
+   file.close();
+}
+
+void createDirectory(string dirName)
+{
+   //boost::filesystem::create_directory(dirName);
+   using namespace boost::lambda;
+   typedef std::istream_iterator<int> in;
+
+   std::for_each(in(std::cin), in(), std::cout << (_1 * 3) << " " );
+}
 
 int main()
 {
@@ -55,6 +73,8 @@ int main()
 	ofstream out_file("../main.cpp");
 	out_file<<contentStream.str()+"boomchika";
 	out_file.close();*/
+
+	createDirectory("TestDir");
 
 	return 0;
 }
